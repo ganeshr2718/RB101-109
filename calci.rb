15f4afcf3b2ccf9ab944fbc83ba149c8ca995e72
nico <ganico369@gmail.com>
2024-01-14 19:56:57 -0500

    Cacli program corrected using rubocop

--- calci.rb
+++ calci.rb
@@ -1,48 +1,48 @@
-def add(num1,num2)
+# frozen_string_literal: true
+
+# Calculator program in ruby
+
+def add(num1, num2)
   Kernel.puts "The sum of the two numbers is #{num1 + num2}"
 end
 
-def sub(num1,num2)
+def sub(num1, num2)
   Kernel.puts "The difference of the two numbers is #{num1 - num2}"
 end
 
-def mul(num1,num2)
+def mul(num1, num2)
   Kernel.puts "The product of the two numbers is #{num1 * num2}"
 end
 
-def div(num1,num2)
-  begin
-    result = num1.to_f / num2.to_f
-    Kernel.puts "The division quotient of the two numbers is #{result}"
-  rescue
-    Kernel.puts "ZeroDivisionError => e"
-  end
+def div(num1, num2)
+  result = num1 / num2
+  Kernel.puts "The division quotient of the two numbers is #{result}"
+rescue ZeroDivisionError => e
+  Kernel.puts "Error is #{e}"
 end
 
-while true
+loop do
   Kernel.puts "Enter what you would like to do or type end to close the program
         1. add
         2. sub
         3. mul
-        4. div
-        "
-  response = Kernel.gets.chomp()
-  if response == "end"
-    return
-  end
-  Kernel.puts "Enter any two numbers"
-  num1 = Kernel.gets.chomp().to_i
-  num2 = Kernel.gets.chomp().to_i
+        4. div "
+  response = Kernel.gets.chomp
+  return if response == 'end'
 
-  if response == "add"
-    add(num1,num2)
-  elsif response == "sub"
-    sub(num1,num2)
-  elsif response == "mul"
-    mul(num1,num2)
-  elsif response == "div"
-    div(num1,num2)
+  Kernel.puts 'Enter any two numbers'
+  num1 = Kernel.gets.chomp.to_i
+  num2 = Kernel.gets.chomp.to_i
+
+  if response == 'add'
+    add(num1, num2)
+  elsif response == 'sub'
+    sub(num1, num2)
+  elsif response == 'mul'
+    mul(num1, num2)
+  elsif response == 'div'
+    div(num1, num2)
   else
-    Kernel.puts " type an appropriate response"
+    Kernel.puts ' type an appropriate response'
   end
 end
